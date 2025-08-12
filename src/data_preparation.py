a08f8f10dd551721b595eca019627bfe739d99c6
Joana <joana.romeiro@hotmail.com>
2025-08-12 17:12:53 +0200

    complete data_preparation

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -7,105 +7,101 @@ import numpy as np
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(message)s')
 
+class LoadAndClean:
+    def load_to_pd(self, input_path):
+        df = pd.read_csv(input_path)
+        return df
 
-def load_to_pd(input_path):
-    df = pd.read_csv(input_path)
-    return df
+    def basic_checks(self, df):
+        print('--------Info--------')
+        print(df.info())
+        print('--------Duplicates--------')
+        print(df.loc[df.duplicated()])
+        print('--------Unique values in categorical variables--------')
+        for col in df.select_dtypes(include='object'):
+            print(col, df[col].nunique())
 
-def basic_checks(df):
-    print(f'--------Info--------')
-    print(df.info())
-    print(f'--------Duplicates--------')
-    print(df.loc[df.duplicated()])
-    print(f'--------Unique values in categorical variables--------')
-    for col in df.select_dtypes(include='object'):
-        print(col, df[col].nunique())
+    # check for nulls -> no null values
+    def check_nulls(self, df):
+        nulls = df.isnull().sum()
+        logging.info(f'Nulls values in: \n{nulls}')
+        if nulls.empty:
+            logging.info(f'No Null values in dataset')
 
-# check for nulls -> no null values
-def check_nulls(df):
-    nulls = df.isnull().sum()
-    logging.info(f'Nulls values in: \n{nulls}')
-    if nulls.empty:
-        logging.info(f'No Null values in dataset')
-
-# As there are some text columns, is best to standardize, (for example, in case of a typo, extra spaces, or caps)
-def clean_text_columns(df):
-    for col in df.select_dtypes(include='object'):
-        df[col] = df[col].str.strip().str.lower()
-    return df
+    # As there are some text columns, is best to standardize, (for example, in case of a typo, extra spaces, or caps)
+    def clean_text_columns(self, df):
+        for col in df.select_dtypes(include='object'):
+            df[col] = df[col].str.strip().str.lower()
+        return df
 
 
-def merge_data(df1, df2, key1, key2, join_type):
-    df = df1.merge(df2, left_on=key1, right_on=key2, how=join_type)
+def merge_data(df1, df2, key1, key2, join):
+    df = df1.merge(df2, left_on=key1, right_on=key2, how=join)
     return df
 
 
 # Detect outliers in the transaction amount -> given that I am trying to detect for fraudulent spikes, suspicious account activity, unusual transaction,
 # and those outliers might be a sign of thay, will not remove them. such patterns might be useful to train the models
-
 def make_boxplot(df, col):
     plt.boxplot(df[col])
     plt.title('Box Plot')
     plt.show()
 
-# Instead of removing the outliers, we will flag them by adding a binary column using Interquartile Range Method
-def flag_outliers(df, col, multiplier=1.5):
-    q1 = df[col].quantile(0.25)
-    q3 = df[col].quantile(0.75)
-    iqr = q3 - q1
-    # Determine outlier boundaries
-    lower_bound = q1 - 1.5 * iqr
-    upper_bound = q3 + 1.5 * iqr
-    df['is_outlier'] = ((df[col] < lower_bound) | (df[col] > upper_bound)).astype(int)
-    return df
+
+class Transform:
+    def data_format(self, df):
+        df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+        df.drop(columns=['Account Number'])
+        df.drop(columns=['Account.1'])
+        df.rename(columns={'Account': 'Account Number'}, inplace =True)
+        return df
 
 
 
-# Fix data types - dates to timestamp
+# Create the object
+loader = LoadAndClean()
 
+df_transactions = loader.load_to_pd('data/processed/transactions.csv')
+loader.basic_checks(df_transactions)
+loader.check_nulls(df_transactions)
+df_transactions = loader.clean_text_columns(df_transactions)
+df_transactions.drop_duplicates(df_transactions) # Remove duplicated as it can skew the models
 
+df_accounts = loader.load_to_pd('data/processed/accounts.csv')
+loader.basic_checks(df_accounts)
+loader.check_nulls(df_accounts)
+df_accounts = loader.clean_text_columns(df_accounts)
 
-df_transactions = load_to_pd('data/processed/transactions.csv')
-df_accounts = load_to_pd('data/processed/accounts.csv')
-basic_checks(df_transactions)
-basic_checks(df_accounts)
-# Remove the 17 transcations duplicated as it can skew the models
-df_transactions = df_transactions.drop_duplicates()
-check_nulls(df_transactions)
-
-df_transactions = clean_text_columns(df_transactions)
-df_accounts = clean_text_columns(df_accounts)
+# Create merged dataset
 df_merged = merge_data(df_transactions, df_accounts, 'Account', 'Account Number', 'left')
-print(df_merged)
-#make_boxplot(df_merged, 'Amount Paid')
-#flag_outliers(df_merged, 'Amount Paid')
-#df_merged.loc[df_merged['is_outlier'] == 1].tail() # Check the outliers rows
 
-# data types and nulls - to convert and fill null values
+# Create the object
+transformer = Transform()
+df_merged = transformer.data_format(df_merged)
 
-#     Column              Dtype  
-# --  ------              -----  
-# 0   Timestamp           object 
-# 1   From Bank           int64  
-# 2   Account             object 
-# 3   To Bank             int64  
-# 4   Account.1           object 
-# 5   Amount Received     float64
-# 6   Receiving Currency  object 
-# 7   Amount Paid         float64
-# 8   Payment Currency    object 
-# 9   Payment Format      object 
-# 10  Is Laundering       int64  
-
-#     Column           Dtype 
-# --  ------          ----- 
-# 0   Bank Name       object
-# 1   Bank ID         int64 
-# 2   Account Number  object
-# 3   Entity ID       object
-# 4   Entity Name     object
-
+make_boxplot(df_merged, 'Amount Paid')
 
 # Data validation and sanity checks for obvious errors - negative amount transaction, transaction with dates in the future
-assert df_transactions['Amount Paid'].min() >= 0, 'Negative transaction amounts found!'
-#assert df_transactions['Timestamp'].to_timestamp <= pd.Timestamp.today(), 'Future transactions'
+assert df_merged['Amount Paid'].min() >= 0, 'Negative transaction amounts found!'
+assert (df_merged['Timestamp'] <= pd.Timestamp.today()).any(), 'Found future transactions'
+
+
+df_merged.info()
+
+
+#   Column              Dtype         
+# ---  ------              -----         
+#  0   Timestamp           datetime64[ns]
+#  1   From Bank           int64         
+#  2   Account Number      object        
+#  3   To Bank             int64         
+#  4   Amount Received     float64       
+#  5   Receiving Currency  object        
+#  6   Amount Paid         float64       
+#  7   Payment Currency    object        
+#  8   Payment Format      object        
+#  9   Is Laundering       int64         
+#  10  Bank Name           object        
+#  11  Bank ID             int64         
+#  12  Entity ID           object        
+#  13  Entity Name         object 
