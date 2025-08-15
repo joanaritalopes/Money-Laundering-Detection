65db10f912c58549192ca244decb773f793f427c
Joana <joana.romeiro@hotmail.com>
2025-08-15 17:35:14 +0200

    pylint fixes

--- src/model_selection.py
+++ src/model_selection.py
@@ -1,3 +1,15 @@
 # 6. Model Selection
 # 7. Model Training
 # 8. Model Evaluation
+
+from imblearn.pipeline import Pipeline
+
+# Approach for the decided model
+
+# pipeline = Pipeline(
+#     steps=[
+#         ('preprocessor', preprocessor),
+#         ('smote', SMOTE(random_state=42)), # Deal with imbalanced data - generate rows for the minority class
+#         ('model', RandomForestClassifier(random_state=42))
+#     ]
+# )
